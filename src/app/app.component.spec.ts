7cad9ff5d4f0722437d6cf60ba3c54954b2f0321
Drew A Reed <drew.reed@hotmail.com>
2024-08-28 22:21:15 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,3 @@
-import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -14,8 +13,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  // This is a simple test that will always pass
-  it('should always pass', () => {
-    expect(true).toBe(true);
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ToDoProjectUI'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ToDoProjectUI');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ToDoProjectUI app is running!');
   });
 });
