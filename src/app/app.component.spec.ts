930f56cd274077311fa0995cd0a75cb0fb753acc
Drew Reed <drew.reed@hotmail.com>
2024-08-28 22:14:23 -0500

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,4 +2,20 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-// All tests have been removed from this file.
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  // This is a simple test that will always pass
+  it('should always pass', () => {
+    expect(true).toBe(true);
+  });
+});
